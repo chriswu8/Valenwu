0cfae2288528c0ea6005aff52f09a41a2b23be38
echen12 <chen.edmond13@gmail.com>
2023-02-28 22:51:00 -0800

    initial commit of db stuff

---
+++ Database/Database.cs
@@ -0,0 +1,31 @@
+﻿using NMemory;
+using NMemory.Tables;
+using System;
+using Valenwu.Tables;
+
+public class MyDatabase : Database
+{
+    public MyDatabase()
+    {
+        /*var peopleTable = base.Tables.Create<Person, int>(p => p.Id);
+        var groupTable = base.Tables.Create<Group, int>(g => g.Id);
+
+        var peopleGroupIdIndex = peopleTable.CreateIndex(
+            new RedBlackTreeIndexFactory<Person>(),
+            p => p.GroupId);
+
+        this.Tables.CreateRelation(
+            groupTable.PrimaryKeyIndex,
+            peopleGroupIdIndex,
+            x => x,
+            x => x);
+
+        this.People = peopleTable;
+        this.Groups = groupTable;*/
+
+        var patientTable = base.Tables.Create<Patient, Guid>(p => p.Id);
+        this.Patients = patientTable;
+    }
+
+    public ITable<Patient> Patients { get; set; }
+}
